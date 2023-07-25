b5a4334bf96034369d135f9eb5c58ab87fd2a838
DavidN250 <onlydavid09@gmail.com>
2023-07-25 15:15:54 +0200

    Latest commit

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react';
 import styles from './Dropdown.module.scss'
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { MdOutlineKeyboardArrowUp } from "react-icons/md";
@@ -11,6 +11,8 @@ import { FaUserCircle } from "react-icons/fa";
 function Dropdown({icon,selected,disabled,onChange}) {
   const userStyle = { color: "blue", fontSize: "1.5em" }
   const selecteStyle = { borderRadius: "1.5em" }
+  const [showDropdownMenu, setShowDropdownMenu] = useState(false)
+
   return (
     <div className={`${styles['dropdown']}`} >
           <button className={
@@ -19,12 +21,13 @@ function Dropdown({icon,selected,disabled,onChange}) {
              ${styles['dropdown-toggle']} 
              ${styles['btn-icon']} 
              ${disabled ? styles['btn-disabled']:null}`}
+             onClick={()=>(setShowDropdownMenu(!showDropdownMenu))}
             type="button" 
             data-bs-toggle="dropdown" 
             aria-expanded="false">
             {icon ? <FaUserCircle style={userStyle} /> : selected ? <span>Action</span>: disabled?<span>Dropdown button</span>:<span>Dropdown button</span>}<MdOutlineKeyboardArrowDown />
           </button>
-          <ul className={`${styles['dropdown-menu']} ${icon ? styles['icon-margin']:''} ${styles['dropdown-hidden']}`}>
+          <ul className={`${styles['dropdown-menu']} ${icon ? styles['icon-margin']:''} ${!showDropdownMenu?styles['dropdown-hidden']:null}`}>
             <li>
               <a className={`${styles['dropdown-item']}`}  href="#">Action</a>
             </li>
